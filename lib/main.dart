63f9cb957019d93b6a2a79a96c36ebb20e33678b
VYSHNAV AJITH <puthen1977@gmail.com>
2023-09-03 23:51:15 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp(home: Text('Hello World')));
+  runApp(const MaterialApp(home: Scaffold(body: Text('Hello World'))));
 }
 
