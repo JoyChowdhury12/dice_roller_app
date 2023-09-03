fa287bd2fdee012fb6a625461d17510222d52828
VYSHNAV AJITH <puthen1977@gmail.com>
2023-09-04 00:40:05 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
             ),
           ),
           child: const Center(
-            child: Text('Hello World!'),
+            child: Text('Hello World!', style: TextStyle(color: Colors.white,fontSize: 28),),
           ),
         ),
       ),
