399986b72a0ca060447efb7a092fa0a5ebb8f07f
VYSHNAV AJITH <puthen1977@gmail.com>
2023-09-04 02:31:22 +0530

    changes

--- lib/text_font.dart
+++ lib/text_font.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class TextFont extends StatelessWidget {
-  const TextFont({super.key});
+  const TextFont(this.text, {super.key});
+
+  final String text;
 
   @override
   Widget build(context) {
-    return const Text(
-      'Hello World!',
-      style: TextStyle(color: Colors.white, fontSize: 28),
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.white, fontSize: 28),
     );
   }
 }
