dfe68ec21f64370c9ad9f6c788f828f5492ab934
VYSHNAV AJITH <puthen1977@gmail.com>
2023-09-04 02:09:30 +0530

    changes

--- lib/gradient_change.dart
+++ lib/gradient_change.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 
-class GradientContainer extends StatelessWidget{
+class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.deepPurple, Colors.black26],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.white, 
-                fontSize: 28
-              ),
-            ),
-          ),
-        );
-      }
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.black26],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: TextFont(),
+      ),
+    );
+  }
+}
+
+class TextFont extends StatelessWidget {
+  const TextFont({super.key});
+
+  @override
+  Widget build(context) {
+    return const Text(
+      'Hello World!',
+      style: TextStyle(color: Colors.white, fontSize: 28),
+    );
+  }
 }
