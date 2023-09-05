27498902188c5e62200971310e01d20a532d3c01
VYSHNAV AJITH <puthen1977@gmail.com>
2023-09-05 15:31:21 +0530

    State set

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class DiceRoller extends StatefulWidget{
+class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
   @override
@@ -9,37 +9,37 @@ class DiceRoller extends StatefulWidget{
   }
 }
 
-class _DiceRollerState extends State<DiceRoller>{
-
+class _DiceRollerState extends State<DiceRoller> {
   var activeDiceImage = 'assets/images/dice-2.png';
 
   void rollDice() {
     setState(() {
-      
+      activeDiceImage = 'assets/images/dice-4.png';
     });
-    activeDiceImage = 'assets/images/dice-4.png';
   }
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            activeDiceImage,
-            width: 200,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            //padding: const EdgeInsets.only(top: 20),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
           ),
-          const SizedBox(height: 20,),
-          TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              //padding: const EdgeInsets.only(top: 20),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize: 28),
-            ),
-            child: const Text('Roll the Dice'),
-          )
-        ],
-      );
+          child: const Text('Roll the Dice'),
+        )
+      ],
+    );
   }
 }
